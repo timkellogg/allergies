a7609b845a5841303bb38eee2d92e70f6e38e48f
Travis Johnston & Tim Kellogg <student@epicodus.com>
2015-08-06 15:19:25 -0700

    finish site functionality

---
+++ app.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/allergies')
+
+get('/') do
+  erb(:form)
+end
+
+get('/allergies') do
+
+  @output = params.fetch("allergy_score").to_i().allergies()
+  erb(:allergies)
+end
---
+++ spec/allergies_integration_spec.rb
@@ -0,0 +1,12 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('using the allergy lookup method', {:type => :feature}) do
+  it('takes the user from inputing an allergy score, to seeing the allergies') do
+    visit('/')
+    fill_in('allergy_score', :with => 3)
+    click_button('Submit')
+    expect(page).to have_content('You are allergic to eggs and peanuts')
+  end
+end
